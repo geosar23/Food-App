806d20b985bd2a3c2e020a438e77dc9641081216
George Saramantis <geosar23@gmail.com>
2021-10-27 19:52:54 +0300

    Menu popup added , FontAwesomeIcon added

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Navigation(){
+    const[showMenu,setMenu]=useState(false)
+
+    let menu
+    let menuMask
+
+    if(showMenu){
+        menu=
+        <div className="bg-gray-200 fixed bg-white top-0 left-0 w-4/5 h-full shadow">
+                Menu
+        </div>
+    }
     return(
-        <nav className="">
-            Nav
+        <nav>
+            <span className="text-xl">
+                <FontAwesomeIcon 
+                    icon={faBars}
+                    onClick={()=>setMenu(!showMenu)} //instead of true , i use the opposite of the current state to make a toggle action 
+                />
+            </span>
+
+            {menu}
         </nav>
     )
 }
