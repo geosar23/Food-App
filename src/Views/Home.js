e9add1e7934edda85e2534a7951bc049c250e8e3
George Saramantis <geosar23@gmail.com>
2021-10-28 18:21:09 +0300

    Imporved error handling with useState

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,10 +1,74 @@
-import React from "react";
+import React ,{useEffect, useState} from "react";
+import axios from 'axios'
+import Loading from "../Components/Loading";
 
 function Home(){
+
+    const [requestError,setError]=useState()
+    const accessToken='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.LBHszzcjG4uVpYR-SpxUUbUEwhz8S8csczNW63L93xM'
+    const url=`https://fe-assignment-server.herokuapp.com/api/v1/food/categories`
+
+    const [products,setProducts]=useState({
+        loading:false,
+        data:null,
+        error:false,
+    })
+
+    
+    useEffect(()=>{
+        setProducts({
+            loading:true,  
+            data:null,
+            error:false,
+        })
+        axios.get(url)
+            .then(response=>{
+                setProducts({
+                    loading:false,
+                    data:response.data,
+                    error:false,
+                })
+            })
+            .catch(err=>{
+                setError(err)
+                setProducts({
+                    loading:false,
+                    data:null,
+                    error:true,
+                })
+            })
+    },[url])
+
+    let content=null
+   
+
+    if(products.error){
+        content=<h1 className="text-center text-3xl bold text-red-500 content-center">Error:{requestError.message} please refresh</h1>
+    }
+
+    if(products.loading){
+        content=<Loading/>
+    }
+
+    if(products.data){
+        content= products.data.map((product,index)=>
+            <div>
+                {product.albumId}
+            </div>
+        )
+    }
+
+
+
+
+
     return(
         <div>
-            <h1 className="font bold text-2xl">This is the home page</h1>
+            <h1 className="font bold text-2xl">Best Sellers</h1>
+
+            {content}
         </div>
+        
     )
 }
 
